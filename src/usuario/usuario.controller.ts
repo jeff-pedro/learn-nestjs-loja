89e80c8d8ec9f169e8dd26f0a3f2c5cc73ccf52b
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-06-25 14:06:14 -0300

    feat: :sparkles: add: validação ao criar usuarios

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsuarioRepository } from './usuario.repository';
+import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
 
 @Controller('/usuarios')
 export class UsuarioController {
     constructor(private usuarioRepository: UsuarioRepository) { };
 
     @Post()
-    async criaUsuario(@Body() dadosDoUsuario) {
+    async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
         await this.usuarioRepository.salvar(dadosDoUsuario);
-        return { message: 'usuario salvo com sucesso!' };
+        return dadosDoUsuario;
     }
 
     @Get()
