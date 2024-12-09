bb813af5e8057723e3e9accb811821b07e732617
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-12-09 18:55:48 -0300

    refactor: :goal_net: captura todos os erros

--- src/filtros/filtro-de-excecao-http.ts
+++ src/filtros/filtro-de-excecao-http.ts
@@ -1,16 +1,28 @@
-import { Catch, ArgumentsHost, HttpException, ExceptionFilter } from '@nestjs/common';
-import { Response } from 'express';
+import { Catch, ArgumentsHost, HttpException, ExceptionFilter, HttpStatus } from '@nestjs/common';
+import { Request, Response } from 'express';
 
-@Catch(HttpException)
+@Catch()
 export class FiltroDeExcecaoHttp implements ExceptionFilter {
-  catch(excecao: HttpException, host: ArgumentsHost) {
+  catch(excecao: unknown, host: ArgumentsHost) {
     console.log(excecao);
 
     const contexto = host.switchToHttp();
     const resposta = contexto.getResponse<Response>();
+    const request = contexto.getRequest<Request>();
 
-    const status = excecao.getStatus();
-    const body = excecao.getResponse();
+    const { status, body } = 
+        excecao instanceof HttpException ?
+        {
+            status: excecao.getStatus(),
+            body: excecao.getResponse()
+        } : {
+            status:HttpStatus.INTERNAL_SERVER_ERROR,
+            body: {
+                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                timestamp: new Date().toISOString(),
+                path: request.url
+            }
+        }
 
     resposta.status(status).json(body);
   }
