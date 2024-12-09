064f2e05c536871e251d9658f72907cbb1fee3f3
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-12-09 18:11:14 -0300

    refactor: :goal_net: cria filtro de exceção

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/postgres.config.service';
 import { ConfigModule } from '@nestjs/config';
 import { PedidoModule } from './pedido/pedido.module';
+import { APP_FILTER } from '@nestjs/core';
+import { FiltroDeExcecaoHttp } from './filtros/filtro-de-excecao-http';
 
 @Module({
   imports: [
@@ -19,6 +21,12 @@ import { PedidoModule } from './pedido/pedido.module';
       inject: [PostgresConfigService]
     }),
   ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: FiltroDeExcecaoHttp
+    }
+  ]
 })
 
 export class AppModule { }
