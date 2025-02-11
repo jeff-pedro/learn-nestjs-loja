c3a58969ae622c7d6deb0c9bac0d61927423a02a
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-02-10 21:18:40 -0300

    obtem o jwt do cabeçalho

--- src/modulos/autenticacao/autenticacao.guard.ts
+++ src/modulos/autenticacao/autenticacao.guard.ts
@@ -1,8 +1,25 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class AutenticacaoGuard implements CanActivate {
-  canActivate(contexto: ExecutionContext): boolean {
+  constructor(private jwtService: JwtService){}
+
+  async canActivate(contexto: ExecutionContext): Promise<boolean> {
+    const requisicao = contexto.switchToHttp().getRequest()
+    
+    const token = this.extrairTokenDoCabecalho(requisicao);
+
+    if(!token) {
+     throw new UnauthorizedException('Error de autenticação');
+    }
+
     return true;
+  }
+
+  private extrairTokenDoCabecalho(requisicao: Request): string | undefined {
+    const [tipo, token] = requisicao.headers.authorization?.split(' ') ?? [];
+    return tipo === 'Bearer' ? token : undefined;
   }
 }
